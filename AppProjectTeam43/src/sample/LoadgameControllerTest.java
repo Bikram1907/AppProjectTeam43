8c7c71053766524694a73aa2d89c1a36187113ba
Rajesh2795 <37520548+Rajesh2795@users.noreply.github.com>
2019-03-03 12:44:10 -0500

    Update LoadgameControllerTest.java

--- AppProjectTeam43/src/sample/LoadgameControllerTest.java
+++ AppProjectTeam43/src/sample/LoadgameControllerTest.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class tests the LoadgameController class methods.
+ */
 class LoadgameControllerTest {
 
+    **
+     * This method sets the objects that are needed to test the methods.
+     */
     @BeforeEach
     void setUp() {
     }
 
+    /**
+     * This method tests if the territory army is increamenting or not when user clicks.
+     */
     @Test
     void mouseclicked() {
     }
@@ -19,6 +28,9 @@ class LoadgameControllerTest {
     void finishedMove() {
     }
 
+    /**
+     * This method tests the if the reinforcement armies are calculated properly or not.
+     */
     @Test
     void calculateReinforcementArmies() {
     }

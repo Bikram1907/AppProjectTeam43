e92b5897373019da35ea528611d7b6cb2ae569f4
manishasharma13 <35543383+manishasharma13@users.noreply.github.com>
2019-03-03 22:28:23 -0500

    Add files via upload

---
+++ AppProjectTeam43/src/sample/MapEditorControllerTest.java
@@ -0,0 +1,32 @@
+package sample;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * This class tests the MapEditorController class methods.
+ */
+class MapEditorControllerTest {
+
+    private File validfile;
+    private File invalidfile;
+    private MapEditorController mecObject = new MapEditorController();
+
+    /**
+     * This method sets the objects needed to test the methods.
+     */
+    @BeforeEach
+    void setUp() {
+
+        validfile = new File("E:\\IntelliJ\\AppProject\\src\\resources\\Maps\\World.map");
+        invalidfile = new File("E:\\IntelliJ\\AppProject\\src\\resources\\Maps\\invalid.map");
+
+        GameDetails.getGamedetails().getgamedetails().add(new GameDetails(validfile,"EDITMAP"));
+        GameDetails.getGamedetails().getgamedetails().add(new GameDetails(invalidfile,"EDITMAP"));
+    }
+}

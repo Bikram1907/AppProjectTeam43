411d809025f9e504c9d9169f61a3155d9c5cfb6c
Rajesh2795 <37520548+Rajesh2795@users.noreply.github.com>
2019-03-05 16:06:08 -0500

    Update Main.java

--- AppProjectTeam43/src/sample/Main.java
+++ AppProjectTeam43/src/sample/Main.java
@@ -7,13 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
-* This is main class that loads the main user interface
-*/
+ * This class is the main class.
+ * This class runs the main GUI window
+ */
 public class Main extends Application {
 
     /**
-    * This method loads the main gui window 
-    */
+     * This method sets the stage and load the main window to the console.
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -23,6 +26,10 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * This is the main method.
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }

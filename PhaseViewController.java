eeb87da49d0d250c9ab5fe9d255144e077f9d010
sargun18 <48069437+sargun18@users.noreply.github.com>
2019-03-24 01:37:38 -0400

    Add files via upload
    PHaseViewController.java class Commit1

---
+++ PhaseViewController.java
@@ -0,0 +1,38 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import sample.model.GameDetails;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * This class controls the phase view window.
+ * This class is responsible for displaying the game current phase and actions happening in the game.
+ * @author Team43
+ */
+public class PhaseViewController implements Observer {
+
+    @FXML
+    public TextArea phaseViewTextArea;
+
+    private static PhaseViewController pvcInstance = new PhaseViewController();
+    private Object currentGamePhase;
+
+    /**
+     * Empty constructor.
+     */
+    public PhaseViewController() {
+    }
+
+    /**
+     * This method by default displays the sample message to the window.
+     */
+    @FXML
+    public void initialize() {
+
+        phaseViewTextArea.setText("This windows displays information about the actions in the phase");
+        phaseViewTextArea.appendText("\n");
+    }
+}

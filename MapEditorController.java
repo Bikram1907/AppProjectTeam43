d3999f7af3e7e52298af0f6a92d04538a23da830
25aanchal <48070428+25aanchal@users.noreply.github.com>
2019-03-25 23:10:18 -0400

    Add files via upload

--- MapEditorController.java
+++ MapEditorController.java
@@ -1,150 +1,23 @@
-package sample;
+package sample.test;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import sample.controller.MapEditorController;
+import sample.model.GameDetails;
 
-import java.io.*;
-import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 
-import basic.GameDetails;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MapEditorController {
+public class MapEditorControllerTest1 {
 
-    @FXML
-    private TextArea textArea;
+    private File validfile;
+    private MapEditorController mecObject = new MapEditorController();
 
-    private File selectedfile;
-    @FXML
-    private AnchorPane anchorPane;
-
-    private StringBuilder builder = new StringBuilder();
-
-    @FXML
-    private Button saveButton;
-
-    @FXML
-    private Button closeButton;
-
-    private File file;
-
-    private String filePath = "E:\\IntelliJ\\AppProject\\src\\resources\\ModifiedMaps\\";
-
-    private Controller controller = new Controller();
-
-    /**
-     * To choose the map.
-     */
-    public void chooseMap() {
-
-        FileChooser chooser = new FileChooser();
-        chooser.setTitle("Open Map");
-        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Map files(*.map)","*.map"));
-        selectedfile = chooser.showOpenDialog(anchorPane.getScene().getWindow());
-        
-        // Check if the Map is valid or not.
-        // If valid print map is valid  else print invalid map.
-        if(ismapValid(selectedfile,"EDITMAP")) {
-            System.out.println("**********************************Map is valid.**************************************");
-        } else {
-            System.out.println("**************************Invalid map check the connections.*************************");
-        }
-
-        // Clear the object created for checking map validation.
-        GameDetails.getGamedetails().clearData();
-        
-        // read the contents from the file and display to the textarea.
-        readFilecontents();
-        textArea.setText(builder.toString());
-    }
-
-    /**
-     * To read the contents from a file.
-     */
-    public void readFilecontents() {
-
-        String line;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(selectedfile));
-            while((line = reader.readLine()) != null) {
-                builder.append(line + "\n");
-            }
-        } catch(Exception e) {
-            System.out.println("Could not load the contents from the file");
-            e.printStackTrace();
-            return;
-        }
-    }
-
-    public void saveContent() {
-
-        file = new File(selectedfile.getName());
-
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(textArea.getText());
-            writer.flush();
-            writer.close();
-
-            boolean result = ismapValid(file, "EDITMAP");
-            if(result) {
-                System.out.println("Map is Valid and successfully saved.");
-                Path path = Paths.get(filePath+selectedfile.getName());
-                try {
-                    writer = Files.newBufferedWriter(path);
-                    writer.write(textArea.getText());
-                    writer.flush();
-                } finally {
-                    if(writer != null) writer.close();
-                    cancelAction();
-                }
-
-            } else {
-                System.out.println("Invalid Map. Remodify the map.");
-
-                // Clear the data.
-                GameDetails.getGamedetails().clearData();
-
-                // Closes the window
-                cancelAction();
-            }
-
-        } catch(Exception e) {
-            System.out.println("Cannot write contents to the file.");
-            e.printStackTrace();
-            return;
-        }
-    }
-    
-    /**
-     * To close the Map editor window.
-     */
-    public void cancelAction() {
-
-        Stage stage = (Stage) closeButton.getScene().getWindow();
-        GameDetails.getGamedetails().clearData();
-        stage.close();
-    }
-
-    public boolean ismapValid(File file, String typename) {
-
-        GameDetails.getGamedetails().getgamedetails().add(new GameDetails(file, typename));
-
-        // To get the index of the Game details object of type map editor.
-        int index = controller.returnIndex(typename);
-
-        GameDetails.getGamedetails().getgamedetails().get(index).createMap(index);
-        boolean result = GameDetails.getGamedetails().getgamedetails().get(index).validateMap(index);
-        System.out.println("Is Map Valid function result is = " + result);
-
-        return result;
+    @Before
+    public void setUp() throws Exception {
+        validfile = new File("E:\\IntelliJ\\AppProject\\src\\resources\\Maps\\invalid.map");
     }
 }
-
-    

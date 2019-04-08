564f93ae31af2c14924e564f5434c39475c8a069
25aanchal <48070428+25aanchal@users.noreply.github.com>
2019-04-07 20:00:27 -0400

    Add files via upload

---
+++ build3/Benevolent.java
@@ -0,0 +1,23 @@
+package sample.strategies;
+
+import javafx.collections.ObservableList;
+import sample.model.Constants;
+import sample.model.Player;
+import sample.model.Territories;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Vector;
+
+/**
+ * This class implements the Strategy interface methods.
+ */
+public class Benevolent implements Strategy {
+
+    /**
+     * An empty constructor.
+     */
+    public Benevolent() {
+    }
+
+}

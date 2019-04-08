776af180cdee495278458b69504eb25bcfa88d82
manishasharma13 <35543383+manishasharma13@users.noreply.github.com>
2019-04-08 14:57:24 -0400

    Add files via upload

---
+++ build3/Strategy.java
@@ -0,0 +1,18 @@
+package sample.strategies;
+
+import javafx.collections.ObservableList;
+import sample.model.Player;
+
+/**
+ * This interface contains methods to handle the phases of the game.
+ */
+public interface Strategy {
+
+    /**
+     * This method implements the reinforcement phase using player strategies.
+     * @param playersList
+     * @param currentPlayer
+     * @return country name
+     */
+    String reinforce(ObservableList<Player> playersList, int currentPlayer);
+

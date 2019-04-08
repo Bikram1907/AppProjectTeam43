4890efb4e1c05b20133dd56eb58a535c7f2c78ff
25aanchal <48070428+25aanchal@users.noreply.github.com>
2019-04-07 22:09:30 -0400

    Update Benevolent.java

--- build3/Benevolent.java
+++ build3/Benevolent.java
@@ -20,4 +20,21 @@ public class Benevolent implements Strategy {
     public Benevolent() {
     }
 
+     /**
+     * This method finds the weakest country to reinforce.
+     * @param playersList
+     * @param player
+     * @return country name
+     */
+    @Override
+    public String reinforce(ObservableList<Player> playersList, int player) {
+
+        String reinforcingCountry = getWeakestTerritory(playersList.get(player).getTerritoriesHeld());
+        if(reinforcingCountry != null) {
+            int noofArmies = playersList.get(player).getPlayerArmies();
+            playersList.get(player).getTerritoriesHeld().get(reinforcingCountry).increaseArmyCountByValue(noofArmies);
+            playersList.get(player).decreaseArmyCountByValue(noofArmies);
+        }
+        return reinforcingCountry;
+    }
 }

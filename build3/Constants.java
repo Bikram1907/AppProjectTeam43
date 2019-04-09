4348449067e16a179382c1688f590710ca52c059
manishasharma13 <35543383+manishasharma13@users.noreply.github.com>
2019-04-08 21:24:01 -0400

    Add files via upload

---
+++ build3/Constants.java
@@ -0,0 +1,11 @@
+package sample.model;
+
+/**
+ * This class contains all the constants that are used in the game.
+ */
+public class Constants {
+
+    public static final String STARTPHASE = "STARTPHASE";
+    public static final String REINFORCEMENTPHASE = "REINFORCEMENTPHASE";
+    public static final String ATTACKPHASE = "ATTACKPHASE";
+}

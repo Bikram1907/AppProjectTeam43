fcbe5dc619f35eefeb91f8bf40c6fe06cc330d9d
Rajesh2795 <37520548+Rajesh2795@users.noreply.github.com>
2019-04-09 01:25:26 -0400

    Update TournamentMode.java

--- build3/TournamentMode.java
+++ build3/TournamentMode.java
@@ -24,4 +24,68 @@ public class TournamentMode {
   private TournamentMode() {
     }
   
+  /**
+     * This method sets the no of maps.
+     * @param noofMaps
+     */
+    public void setNoofMaps(int noofMaps) {
+        this.noofMaps = noofMaps;
+    }
+
+    /**
+     * This method sets the no of games.
+     * @param noofGames
+     */
+    public void setNoofGames(int noofGames) {
+        this.noofGames = noofGames;
+    }
+
+    /**
+     * This method sets the no of players.
+     * @param noofPlayers
+     */
+    public void setNoofPlayers(int noofPlayers) {
+        this.noofPlayers = noofPlayers;
+    }
+
+    /**
+     * This method sets the results.
+     * @param results
+     */
+    public void setResults(ArrayList<ArrayList<String>> results) {
+        this.results = results;
+    }
+
+    /**
+     * To set the index
+     * @param index
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    /**
+     * Stores the user input map files
+     * @param mapFiles
+     */
+    public void setMapFiles(ArrayList<File> mapFiles) {
+        this.mapFiles = mapFiles;
+    }
+
+    /**
+     * Stores the user input characters.
+     * @param playerCharacters
+     */
+    public void setPlayerCharacters(HashMap<String, String> playerCharacters) {
+        this.playerCharacters = playerCharacters;
+    }
+
+    /**
+     * Stores the no of turns.
+     * @param noofTurns
+     */
+    public void setNoofTurns(int noofTurns) {
+        this.noofTurns = noofTurns;
+    }
+  
 }

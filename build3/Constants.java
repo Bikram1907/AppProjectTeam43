b7eec977a7933ef6df4e906b0d94974de2f91963
manishasharma13 <35543383+manishasharma13@users.noreply.github.com>
2019-04-08 21:25:11 -0400

    Update Constants.java

--- build3/Constants.java
+++ build3/Constants.java
@@ -8,4 +8,8 @@ public class Constants {
     public static final String STARTPHASE = "STARTPHASE";
     public static final String REINFORCEMENTPHASE = "REINFORCEMENTPHASE";
     public static final String ATTACKPHASE = "ATTACKPHASE";
+    public static final String FORTIFICATIONPHASE = "FORTIFICATIONPHASE";
+    public static final String ENDPHASE = "ENDPHASE";
+    public static final String WINNER = "WINER";
+
 }

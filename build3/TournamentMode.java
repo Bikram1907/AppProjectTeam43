99ef72e4e6e0d133b972e2a75e446d318f32413e
Rajesh2795 <37520548+Rajesh2795@users.noreply.github.com>
2019-04-09 01:22:44 -0400

    Update TournamentMode.java

--- build3/TournamentMode.java
+++ build3/TournamentMode.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
  * This class will store the tournament mode inputs and results.
  */
 public class TournamentMode {
-
   
+  private static TournamentMode ourInstance = new TournamentMode();
+
+    public static TournamentMode getInstance() {
+        return ourInstance;
+    }
 }
